8ac8e1ee8af5508613d1a24ca51273aab16e7885
Deep Bindal <deepbindal36@gmail.com>
2024-10-13 16:28:49 +0530

    initial commit

---
+++ lib/mongodb/models/event.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, model, models, Schema } from "mongoose";
+
+
+export interface IEvent extends Document {
+    _id: string;
+    title: string;
+    description?: string;
+    location?: string;
+    createdAt: Date;
+    imageUrl: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    price: string;
+    isFree: boolean;
+    category: { _id: string, name: string }
+    organizer: { _id: string, firstName: string, lastName: string }
+  }
+
+const eventSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String},
+    location: {type: String},
+    photo: {type: String},
+    createdAt : {type: Date, default: Date.now},
+    startDateTime: {type: Date, default: Date.now},
+    endDateTime: {type: Date, default: Date.now},
+    price: {type: String},
+    isFree: {type: Boolean, default: false},
+    organizer: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    category: {type: mongoose.Schema.Types.ObjectId, ref: "Category"},
+})
+
+const Event = models.Event || model("Event", eventSchema);
+
+export default Event;
